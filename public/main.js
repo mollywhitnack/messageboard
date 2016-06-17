3476104a349f9b602a19b43e6d73982b751798e9
mollywhitnack <mollywhitnack@gmail.com>
2016-06-16 21:39:05 -0700

    done for tongith

--- public/main.js
+++ public/main.js
@@ -10,9 +10,14 @@ function init(){
   loadPage();
   $('.addNewMessage').click(addNewMessage);
   $('.submitMessage').click(submitMessage);
+  $('.messageBoardArea').on('click', '.deleteMessage',  deleteMessage);
+  $('.messageBoardArea').on('click', '.updateMessage',  updateMessage);
+ //$('.toDoList').on('click', '.Done',  taskCheck);
+
 }
 
-  
+
+
 function loadPage(){
 
 $.get('/messages')
@@ -23,11 +28,14 @@ $.get('/messages')
       var $span = $('.template').clone();
       var author = data[i].author;
       var text = data[i].text;
-
-      console.log("author: " , author);
-      console.log("text: " , text);
+      var id = data[i].id;
+      console.log("id: ", id);
+      //console.log("author: " , author);
+      //console.log("text: " , text);
       $span.find('.authorTemp').text(author);
       $span.find('.messageTemp').text(text);
+      $span.find('.deleteMessage').data('id', id);
+      $span.find('.updateMessage').data('id', id);
       $span.removeClass('template');
       $messages.push($span);
     }
@@ -72,6 +80,73 @@ $.ajax({
 
 });
 }
+
+function deleteMessage(){
+  console.log("In delete");
+  console.log($(this).data('id'));
+  var id = $(this).data('id');
+
+  var urly = `/messages/${id}`
+
+  $.ajax({
+    url: urly,
+    type: 'DELETE',
+    success: function(data, status)
+    {
+      //console.log("data: " , data);
+      loadPage();
+        //data - response from server
+    },
+    error: function (jqXHR, status, error)
+    {
+       console.log("error: " , error);
+       console.log("status: " , status);
+ 
+    }
+  });
+
+}
+
+function updateMessage(){
+  console.log("HERE");
+  $('.showInput').show();
+  $('.addNewMessage').hide();
+}
+
+function submitUpdate(){
+  console.log("In edit");
+  console.log($(this).data('id'));
+  var id = $(this).data('id');
+
+  var urly = `/messages/${id}`
+  /*$.ajax({
+    url: '/echo/html/',
+    type: 'PUT',
+    data: "name=John&location=Boston",
+    success: function(data) {
+    alert('Load was performed.');
+  }
+  });*/
+}
+
+ /* $.ajax({
+    url : '/messages',
+    type: "POST",
+    data : formData,
+    success: function(data, textStatus, jqXHR)
+    {
+      console.log("data: " , data);
+      loadPage();
+        //data - response from server
+    },
+    error: function (jqXHR, status, error)
+    {
+       console.log("error: " , error);
+       console.log("status: " , status);
+ 
+    }
+
+});*/
 
 
 
